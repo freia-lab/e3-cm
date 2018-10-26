9694084d9b5ac238278094d492edab03fcece8a3
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2018-10-26 17:31:43 +0200

    First connection to Keithley.

--- startup/cm-freia.cmd
+++ startup/cm-freia.cmd
@@ -1,4 +1,5 @@
 #require autosave
+require streamdevice,2.7+
 
 epicsEnvSet("AUTOSAVE_SYSM_PV_PREFIX","cm:")
 
@@ -6,7 +7,7 @@ epicsEnvSet("AUTOSAVE_SYSM_PV_PREFIX","cm:")
 ## Register all support components         ##
 #############################################
 
-requireSnippet(userPreDriverConf-cm-freia.cmd, IP_ADDR=$(PLC_IPADDR))
+requireSnippet(userPreDriverConf-cm-freia.cmd, "IP_ADDR=$(PLC_IPADDR), IP_ADDR2=$(KEITH_IPADDR)")
 sleep 2
 requireSnippet(dbToLoad-cm-freia.cmd)
 #requireSnippet(cm-freia-preSaveRestore.cmd)
