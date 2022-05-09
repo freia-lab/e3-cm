0ecbf9a56f85205d82769ebf59bfab539062b972
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-05-09 14:39:53 +0200

    make works ok when changes in calibration directory are committed. Installed in cellMods but not tested.

--- cmds/cm-freia-postSaveRestore.cmd
+++
@@ -1,6 +0,0 @@
-#############################################
-## Autosave monitor post setup             ##
-#############################################
-
-create_monitor_set("ioc-cm-freia.req",30,"P=$(AUTOSAVE_SYSM_PV_PREFIX)")
-
--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,3 +1,15 @@
 # This should be a test or example startup script
 
 require cm
+require recsync
+require iocstats
+
+epicsEnvSet("IOCNAME", "ioc17-cm")
+
+iocshLoad("$(cm_DIR)/cm.iocsh", "PLC_IP=192.168.1.11, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
+
+# Start recsync client
+#iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
